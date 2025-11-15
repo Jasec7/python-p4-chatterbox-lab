9c7a72a92d2202f40032f0b280f88e99532afe42
Jasec <jasec_7@hotmail.com>
2025-11-15 03:22:51 -0800

    complete python p4 chatterbox lab

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+from ast import Return
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
@@ -11,16 +12,60 @@ app.json.compact = False
 
 CORS(app)
 migrate = Migrate(app, db)
-
+    
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages',methods=['GET','POST'])
 def messages():
-    return ''
-
-@app.route('/messages/<int:id>')
+    
+    if request.method == 'GET':
+        msn =  [message.to_dict() for message in Message.query.order_by(Message.created_at).all()]
+        response = make_response(msn, 200)
+        return response 
+        
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_message =   Message(
+            body=data["body"],
+            username=data["username"]
+        )
+            
+        db.session.add(new_message)
+        db.session.commit()
+        
+        new_message_dict = new_message.to_dict()
+        response = make_response(new_message_dict, 201)
+        return response
+        
+@app.route('/messages/<int:id>',methods=['PATCH','DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+
+    if request.method == 'PATCH':
+        data = request.get_json()
+        for attr in data:
+            setattr(message, attr, data.get(attr))
+        
+        db.session.add(message)
+        db.session.commit()
+
+        message_dict = message.to_dict()
+
+        response= make_response(message_dict, 200)
+
+        return response
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "The message was deleted."
+        }
+
+        response = make_response(response_body, 200)
+        return response
 
 if __name__ == '__main__':
     app.run(port=5555)
